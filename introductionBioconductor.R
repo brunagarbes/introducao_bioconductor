5e065cad01efd0d024b88dfab25e7c42d795d55d
Garbes <brugarbes@gmail.com>
2019-02-02 21:34:16 -0200

    First commit

---
+++ introductionBioconductor.R
@@ -0,0 +1,10 @@
+#What is bioconductor?
+
+# Load the BiocInstaller package
+library(BiocInstaller)
+
+# Explicit syntax to check the Bioconductor version
+BiocInstaller::biocVersion()
+
+# When BiocInstaller is loaded use biocVersion alone
+biocVersion()
